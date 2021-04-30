0ae6e610d3b80aac24982475eb4620c6311b4213
tirth2000p <80301319+tirth2000p@users.noreply.github.com>
2021-04-30 17:33:49 -0400

    Delete stub1.java

--- src/stub1.java
+++
@@ -1,9 +0,0 @@
-import java.io.IOException;
-
-public class stub1 
-{
-	public static void main(String []args) throws IOException, InterruptedException 
-	{
-		System.out.println("You have reachec stub 1");
-	}
-}
