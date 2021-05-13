91f661d1b27a3b1d60e4c4a33debcd46ea1d9f62
tirth2000p <80301319+tirth2000p@users.noreply.github.com>
2021-05-13 08:13:07 -0400

    Add files via upload

---
+++ Application/app/src/main/java/com/example/myapplication/SecondFragment.java
@@ -0,0 +1,47 @@
+package com.example.myapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+class SameNameConflicts{
+    Fragment fragment;
+    android.app.Fragment appFragment;
+}
+public class SecondFragment extends Fragment {
+    final static String DATA_RECEIVE = "data_receive";
+    TextView showReceivedData;
+    @Override
+    public View onCreateView(
+            LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState
+    ) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_second, container, false);
+    }
+
+    public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        view.findViewById(R.id.AF2pre).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_FirstFragment);
+            }
+        });
+        view.findViewById(R.id.F2next).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                NavHostFragment.findNavController(SecondFragment.this)
+                        .navigate(R.id.action_SecondFragment_to_ThirdFragment);
+            }
+        });
+    }
+}
